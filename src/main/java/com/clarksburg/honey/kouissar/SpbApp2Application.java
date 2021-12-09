bf1d5c07f793ce854bf7d53feb91c1e5c32143ce
kouissar <kouissar@gmail.com>
2021-12-08 23:29:57 -0500

    adding security - adding actuator

--- src/main/java/com/clarksburg/honey/kouissar/SpbApp2Application.java
+++ src/main/java/com/clarksburg/honey/kouissar/SpbApp2Application.java
@@ -2,8 +2,9 @@ package com.clarksburg.honey.kouissar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class SpbApp2Application {
 
 	public static void main(String[] args) {
